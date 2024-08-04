9ba0a6cbc8b3bba26069bde0c54dce3c27e89048
MDARIFULISLAM13 <mdarifulislamncs13@gmail.com>
2024-08-05 03:35:45 +0600

    finaly solved

--- PREV BATCH/Module 3/Printing_some_primes.cpp
+++ PREV BATCH/Module 3/Printing_some_primes.cpp
@@ -1,8 +1,8 @@
 /**
  *
  * Author : Md.Ariful Islam
- * Date : 02-08-2024
- * time : 23:18:05
+ * Date : 05-08-2024
+ * time : 03:20:42
  *
  **/
 #include <bits/stdc++.h>
@@ -16,33 +16,41 @@ using ll = long long int;
 #define ull unsigned long long
 #define vec vector<ll>
 const int mx = 1e8;
-bitset<mx> isprime;
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    isprime.set();
-    int lim = 1e8;
 
-    for (int i = 3; i <= lim; i++)
-    {
-        isprime[i] = 1;
-    }
+    int lim = 1e8;
+    bitset<mx> ip;
+    vector<int> p;
     for (int i = 3; i <= lim; i += 2)
     {
-        for (int j = i + i; j <= lim; j += i)
+        ip[i] = true;
+    }
+
+    for (int i = 3; i <= sqrt(lim); i += 2)
+    {
+        if (ip[i] == true)
         {
-            isprime[j] = 0;
+            for (int j = i + i; j <= lim; j += i)
+            {
+                ip[j] = false;
+            }
         }
     }
-    cout << "2\n";
+    p.push_back(2);
     for (int i = 3; i <= lim; i += 2)
     {
-        if (isprime[i] == 1)
+        if (ip[i] == true)
         {
-            cout << i << endl;
+            p.push_back(i);
         }
+    }
+    for (int i = 0; i < p.size(); i += 100)
+    {
+        cout << p[i] << endl;
     }
 
     return 0;
