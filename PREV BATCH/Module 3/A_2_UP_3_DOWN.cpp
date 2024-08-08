59fcfaeead243d52f5914f3f32d7bb0d1dbc5853
MDARIFULISLAM13 <mdarifulislamncs13@gmail.com>
2024-08-08 23:55:37 +0600

    ok

---
+++ PREV BATCH/Module 3/A_2_UP_3_DOWN.cpp
@@ -0,0 +1,37 @@
+/**
+ *
+ * Author : Md.Ariful Islam
+ * Date : 08-08-2024
+ * time : 22:25:00
+ *
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define endl "\n";
+#define ft float
+#define du double
+#define ull unsigned long long
+#define vec vector<ll>
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int x, y;
+    cin >> x >> y;
+    if (x > y)
+    {
+        cout << "YES\n";
+    }
+    else
+    {
+        cout << "No\n";
+    }
+
+    return 0;
+}
