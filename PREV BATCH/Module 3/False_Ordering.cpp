dc089505ae6ac40a061838c04d56e7d05532027e
MDARIFULISLAM13 <mdarifulislamncs13@gmail.com>
2024-08-27 03:20:42 +0600

    complete

--- PREV BATCH/Module 3/False_Ordering.cpp
+++ PREV BATCH/Module 3/False_Ordering.cpp
@@ -21,6 +21,27 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
+    vector<int> dv(1001, 0);
+    for (int i = 1; i <= 1000; i++)
+    {
+        for (int j = i; j <= 1000; j += i)
+        {
+            dv[j]++;
+        }
+    }
+    vector<pair<int, int>> ans;
+    for (int i = 0; i <= 1000; i++)
+    {
+        ans.push_back({dv[i], (i * -1)});
+    }
+    sort(ans.begin(), ans.end());
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        int v;
+        cin >> v;
+        cout << "Case " << i << ": " << (ans[v].second * -1) << endl;
+    }
     return 0;
 }
