33d760e634a6fc2a7b79c515f8e1b7130e8fc206
MDARIFULISLAM13 <mdarifulislamncs13@gmail.com>
2024-09-08 20:03:50 +0600

    ok

--- PREV BATCH/STL Series/Map/map.cpp
+++ PREV BATCH/STL Series/Map/map.cpp
@@ -1,1 +1,33 @@
-   
+/**
+ *
+ * Author : Md.Ariful Islam
+ * Date : 08-09-2024
+ * time : 19:58:24
+ *
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define endl "\n";
+#define ft float
+#define du double
+#define ull unsigned long long
+#define vec vector<ll>
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    map<string, int> id;
+    id["Arif"] = 1;
+    id["Akhi"] = 2;
+    id["Rupa"] = 3;
+    id["Araf"] = 4;
+    cout << id["Arif"];
+
+    return 0;
+}
