a3653ee735759dd1e490313890296902d009283f
MDARIFULISLAM13 <mdarifulislamncs13@gmail.com>
2024-09-12 14:25:39 +0600

    try

---
+++ PREV BATCH/STL Series/Map/Find the Frequency.cpp
@@ -0,0 +1,29 @@
+/**
+ *
+ * Author : Md.Ariful Islam
+ * Date : 12-09-2024
+ * time : 14:25:05
+ *
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define endl "\n";
+#define ft float
+#define du double
+#define ull unsigned long long
+#define vec vector<ll>
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll n;
+    cin >> n;
+
+    return 0;
+}
