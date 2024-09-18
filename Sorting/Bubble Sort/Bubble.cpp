7563faf0b13150b84915484a197abd205c924952
MDARIFULISLAM13 <mdarifulislamncs13@gmail.com>
2024-09-19 04:07:19 +0600

    bubble

---
+++ Sorting/Bubble Sort/Bubble.cpp
@@ -0,0 +1,51 @@
+/**
+ *
+ * Author : Md.Ariful Islam
+ * Date : 19-09-2024
+ * time : 02:45:36
+ *
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define endl "\n";
+#define ft float
+#define du double
+#define ull unsigned long long
+#define vec vector<ll>
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int count = 1;
+    while (count < n)
+    {
+        for (int i = 0; i < n - count; i++)
+        {
+            if (arr[i] > arr[i + 1])
+            {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+
+        count++;
+    }
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j] << " ";
+    }
+    return 0;
+}
