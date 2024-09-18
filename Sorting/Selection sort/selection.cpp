c735973a9bd94a5a3ecb626da4151f4be26809f9
MDARIFULISLAM13 <mdarifulislamncs13@gmail.com>
2024-09-19 04:14:06 +0600

    selection complete

---
+++ Sorting/Selection sort/selection.cpp
@@ -0,0 +1,48 @@
+/**
+ *
+ * Author : Md.Ariful Islam
+ * Date : 19-09-2024
+ * time : 04:10:55
+ *
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define endl "\n";
+#define ft float
+#define du double
+#define ull unsigned long long
+#define vec vector<ll>
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    int a[n];
+    for (auto i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (a[j] < a[i])
+            {
+                swap(a[i], a[j]);
+            }
+        }
+    }
+    for (auto i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+
+    return 0;
+}
