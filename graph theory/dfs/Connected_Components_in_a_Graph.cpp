91051937f0a73c8b4ff105697e26d1c17b533ab0
MDARIFULISLAM13 <mdarifulislamncs13@gmail.com>
2024-10-03 13:43:23 +0600

    dfs

---
+++ graph theory/dfs/Connected_Components_in_a_Graph.cpp
@@ -0,0 +1,61 @@
+/**
+ *
+ * Author : Md.Ariful Islam
+ * Date : 03-10-2024
+ * time : 13:14:04
+ *
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define endl "\n";
+#define ft float
+#define du double
+#define ull unsigned long long
+#define vec vector<ll>
+const int mx = 1e5 + 123;
+vector<int>v[mx];
+vector<bool>ck(mx, 0);
+int cnt = 0;
+int mxcnt = 0;
+void dfs(int i) {
+    cnt++;
+    ck[i] = 1;
+    for (auto u : v[i]) {
+        if (ck[u] == 0) {
+            dfs(u);
+        }
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1;i <= m;i++) {
+        int x, y;
+        cin >> x >> y;
+        v[x].push_back(y);
+    }
+    int ans = 0;
+    for (int i = 1;i <= n;i++) {
+        if (ck[i] == 0) {
+            ans++;
+            dfs(i);
+            mxcnt = max(cnt, mxcnt);
+        }
+        else {
+            cnt = 0;
+        }
+
+    }
+    cout << ans << endl;
+    //for find out max connect compunent
+    // cout << mxcnt;
+    return 0;
+}
