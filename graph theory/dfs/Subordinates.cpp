6e888f46962d4e17d47285a9c7e65c565f39529c
MDARIFULISLAM13 <mdarifulislamncs13@gmail.com>
2024-12-10 00:37:19 +0600

    solve from cses using dfs

--- graph theory/dfs/Subordinates.cpp
+++ graph theory/dfs/Subordinates.cpp
@@ -1,8 +1,8 @@
 /**
  *
  * Author : Md.Ariful Islam
- * Date : 09-12-2024
- * time : 23:59:30
+ * Date : 10-12-2024
+ * time : 00:24:44
  * Problem Name : Subordinates
  *
  **/
@@ -21,14 +21,35 @@ using ll = long long int;
 #define ull unsigned long long
 #define vec vector<ll>
 #define mem(dp,i) memset(dp,i,sizeof(dp));
+const int mx = 1e6;
+vector<int> m[mx];
+vector<int>sub;
+int dfs(int i) {
 
+    int cnt = 0;
+    for (auto x : m[i]) {
+        cnt += 1 + dfs(x);
+    }
+    sub[i] = cnt;
+    return cnt;
+}
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-
-
+    int n;
+    cin >> n;
+    for (int i = 2;i <= n;i++) {
+        int boss;
+        cin >> boss;
+        m[boss].push_back(i);
+    }
+    sub.resize(n + 1, 0);
+    dfs(1);
+    for (int i = 1;i <= n;i++) {
+        cout << sub[i] << " ";
+    }
     return 0;
 }
