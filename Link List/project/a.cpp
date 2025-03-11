417050e130055ad5686910f93fe153b87dae888e
MDARIFULISLAM13 <mdarifulislamncs13@gmail.com>
2025-03-11 09:34:42 +0600

    Basic Blockchain Structure Done

--- Link List/project/a.cpp
+++ Link List/project/a.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 void load();
 void log_in();
+void clear()
+{
+    system("cls");
+};
 
 class All_Transactions
 {
@@ -19,8 +23,47 @@ private:
     All_Transactions *tail = NULL;
 
 public:
+    void save_All_transactions()
+    {
+        ofstream file("transactions.txt", ios::app);
+        if (file.is_open())
+        {
+            file << head->from << " " << head->to << " " << head->Transaction_value << " " << head->hash << endl;
+            file.close();
+        }
+    }
+    void load_All_transactions()
+    {
+        ifstream file("transactions.txt");
+        if (file.is_open())
+        {
+            string from, to, hash;
+            int value;
+            while (file >> from >> to >> value >> hash)
+            {
+                All_Transactions *at = new All_Transactions();
+                at->from = from;
+                at->to = to;
+                at->Transaction_value = value;
+                at->hash = hash;
+                at->next_block = NULL;
+
+                if (head == NULL)
+                {
+                    head = tail = at;
+                }
+                else
+                {
+                    tail->next_block = at;
+                    tail = at;
+                }
+            }
+            file.close();
+        }
+    }
     string set_data(string from, string to, int value)
     {
+
         All_Transactions *at = new All_Transactions();
         at->from = from;
         at->to = to;
@@ -39,14 +82,21 @@ public:
             tail->next_block = at;
             tail = at;
         }
+        save_All_transactions();
         return hash;
     }
     void print_data()
     {
+        clear();
+        load_All_transactions();
         All_Transactions *temp = head;
         All_Transactions *pre = NULL;
         int cnt = 1;
-
+        if (temp == NULL)
+        {
+            cout << "No transactions Done\n\n";
+            return;
+        }
         while (temp != NULL)
         {
             cout << "Block No : " << cnt << endl;
@@ -60,14 +110,17 @@ public:
             pre = temp;
 
             temp = temp->next_block;
+            cout << endl;
         }
     }
     void search_by_hash()
     {
+        load_All_transactions();
+        clear();
         cout << "Enter Your Hash Id\n";
         string val;
         cin >> val;
-
+        cout << endl;
         All_Transactions *temp = head;
         All_Transactions *pre = NULL;
         int cnt = 1;
@@ -81,7 +134,7 @@ public:
                 cout << "Transaction_value : " << temp->Transaction_value << endl;
                 cout << "Hash : " << temp->hash << endl;
                 cout << "Previous Block : " << pre << endl;
-                cout << "Next Block : " << temp->next_block << endl;
+                cout << "Next Block : " << temp->next_block << "\n\n";
                 load();
             }
 
@@ -89,9 +142,15 @@ public:
             pre = temp;
             temp = temp->next_block;
         }
+
+        {
+            cout << "No transactions Found\n\n";
+            return;
+        }
     }
 };
 All_Transactions_controler atc;
+
 class Account
 {
 private:
@@ -121,13 +180,30 @@ public:
 
     void set_security(int code, Account *temp)
     {
+        clear();
         temp->security_code = code;
         cout << "Your Account Created Sucessfully.\n";
-        cout << "Your Security Code is " << code << " . Save this for next time log in\n";
+        cout << "Your Security Code is " << code << " . Save this for next time log in\n\n";
     }
     bool security_ck(int code, Account *temp)
     {
         return temp->security_code == code ? true : false;
+    }
+    int get_code(Account *temp)
+    {
+        return temp->security_code;
+    }
+    void set_code(Account *temp, int code)
+    {
+        temp->security_code = code;
+    }
+    int get_balance(Account *temp)
+    {
+        return temp->Account_balance;
+    }
+    void set_balance(Account *temp, int val)
+    {
+        temp->Account_balance = val;
     }
 };
 void log_in_load(Account *temp);
@@ -142,6 +218,120 @@ private:
 public:
     Account *temp = head;
 
+    void save_All_Account(Account *temp, bool overwrite = false)
+    {
+        ofstream file("AccountInfo.txt", ios::app);
+        if (file.is_open())
+        {
+            file << temp->Account_address << " " << temp->Account_password << " "
+                 << temp->deposit_address << " " << temp->get_code(temp) << " "
+                 << temp->get_balance(temp);
+
+            file << endl;
+            file.close();
+        }
+    }
+
+    void update_amount_withdraw(const string &account_address, int additional_balance)
+    {
+        ifstream file("AccountInfo.txt");
+        stringstream updated_content;
+        string line;
+        bool updated = false;
+        while (getline(file, line))
+        {
+            stringstream ss(line);
+            string Account_Add, Account_Pass, Deposit_add;
+            int balance, code;
+
+            ss >> Account_Add >> Account_Pass >> Deposit_add >> code >> balance;
+
+            if (account_address == Deposit_add)
+            {
+                balance += additional_balance;
+                updated = true;
+            }
+
+            updated_content << Account_Add << " " << Account_Pass << " " << Deposit_add << " " << code << " " << balance << endl;
+        }
+
+        file.close();
+
+        if (updated)
+        {
+            ofstream outfile("AccountInfo.txt", ios::trunc);
+            outfile << updated_content.str();
+            outfile.close();
+        }
+    }
+    void update_amount_deposit(const string &account_address, int additional_balance)
+    {
+        ifstream file("AccountInfo.txt");
+        stringstream updated_content;
+        string line;
+        bool updated = false;
+        while (getline(file, line))
+        {
+            stringstream ss(line);
+            string Account_Add, Account_Pass, Deposit_add;
+            int balance, code;
+
+            ss >> Account_Add >> Account_Pass >> Deposit_add >> code >> balance;
+
+            if (account_address == Deposit_add)
+            {
+                balance -= additional_balance;
+                updated = true;
+            }
+
+            updated_content << Account_Add << " " << Account_Pass << " " << Deposit_add << " " << code << " " << balance << endl;
+        }
+
+        file.close();
+
+        if (updated)
+        {
+            ofstream outfile("AccountInfo.txt", ios::trunc);
+            outfile << updated_content.str();
+            outfile.close();
+        }
+    }
+
+    void load_All_Account()
+    {
+        ifstream file("AccountInfo.txt");
+        if (file.is_open())
+        {
+            string Account_Add, Account_Pass, Deposit_add;
+            int balance, code, data_size;
+
+            while (file >> Account_Add >> Account_Pass >> Deposit_add >> code >> balance)
+            {
+                Account *na = new Account();
+                na->Account_address = Account_Add;
+                na->Account_password = Account_Pass;
+                na->deposit_address = Deposit_add;
+                na->set_balance(na, balance);
+                na->set_code(na, code);
+                if (head == NULL)
+                {
+                    head = tail = na;
+                }
+                else
+                {
+                    tail->next_block = na;
+                    tail = na;
+                }
+            }
+            file.close();
+        }
+    }
+
+    Account_info()
+    {
+        load_All_Account();
+    }
+
     void create_Account(string user_id, string pass)
     {
         Account *na = new Account();
@@ -152,7 +342,7 @@ public:
         auto now = chrono::system_clock::now();
         auto timestamp = chrono::duration_cast<chrono::milliseconds>(now.time_since_epoch()).count();
         mt19937 gen(timestamp);
-        uniform_int_distribution<int> dis(1, 9);
+        uniform_int_distribution<int> dis(1111, 9999);
         int code = dis(gen);
 
         na->set_security(code, na);
@@ -169,6 +359,7 @@ public:
             tail->next_block = na;
             tail = na;
         }
+        save_All_Account(na);
     }
 
     bool ck_user_exist(string address)
@@ -218,11 +409,16 @@ public:
                 vec.push_back("Amount : " + to_string(val));
                 vec.push_back("Hash : " + HASH);
                 temp->data.push_back(vec);
+                update_amount_withdraw(Address, val);
 
                 break;
             }
             temp = temp->next_block;
         }
+    }
+    void withdraw_Balance(string Address, int balance)
+    {
+        update_amount_deposit(Address, balance);
     }
 };
 
@@ -233,12 +429,14 @@ class account_activity : public Account
 public:
     void Show_balance(Account *temp)
     {
+        clear();
         int balance = show_balance(temp);
         cout << "Your Account Balance is : " << balance << endl;
         log_in_load(temp);
     }
     void withdraw(Account *temp)
     {
+        clear();
         cout << "Enter Withdraw Address\n";
         string Address;
         cin >> Address;
@@ -260,8 +458,8 @@ public:
         vec.push_back(data_address);
         vec.push_back("Amount : " + to_string(val));
         string address_deposit = temp->deposit_address;
-
-        string HASH = atc.set_data(deposit_address, Address, val);
+        ac.withdraw_Balance(address_deposit, val);
+        string HASH = atc.set_data(address_deposit, Address, val);
         vec.push_back("Hash : " + HASH);
         temp->data.push_back(vec);
         ac.Add_Balance(Address, val, address_deposit, HASH);
@@ -269,8 +467,10 @@ public:
     }
     void My_All_Transactions(Account *temp)
     {
+        clear();
         if (temp->data.size() == 0)
         {
+
             cout << "No Transactions Complete\n";
             log_in_load(temp);
             return;
@@ -279,7 +479,7 @@ public:
         cout << "Transactions Details\n";
         for (int i = 0; i < temp->data.size(); i++)
         {
-            cout << i + 1 << ": ";
+            cout << i + 1 << ": \n";
             for (auto j : temp->data[i])
             {
                 cout << j << endl;
@@ -291,6 +491,7 @@ public:
 
 void log_in()
 {
+    clear();
     cout << "Enter User id\n";
     string id;
     cin >> id;
@@ -304,18 +505,22 @@ void log_in()
     Account *temp = ac.log_in_ck(id, code, pass);
     if (temp != NULL)
     {
-        cout << "Log in Successfully\n";
+
+        clear();
+        cout << "Log in Successfully\n\n";
 
         log_in_load(temp);
     }
     else
     {
-        cout << "Wrong User id / Security Code / Password\n";
+        clear();
+        cout << "Wrong User id / Security Code / Password\n\n";
         load();
     };
 }
 void create_account_page()
 {
+    clear();
     cout << "Enter User id\n";
     string user_id;
     cin >> user_id;
@@ -344,7 +549,8 @@ void create_account_page()
 
 void load()
 {
-    cout << "1..view all Transactions\n2..Search By Hash\n3..Log in\n4..Create Account\n5..Exit\n";
+
+    cout << "\n1..view all Transactions\n2..Search By Hash\n3..Log in\n4..Create Account\n5..Exit\n\n";
     int x;
     cin >> x;
     if (x == 1)
@@ -379,8 +585,9 @@ void load()
 }
 void log_in_load(Account *temp)
 {
+
     account_activity acc;
-    cout << "1..Account Balance\n2..Deposit Address\n3..Withdraw\n4..My All Transactions\n5..Log Out\n";
+    cout << "\n1..Account Balance\n2..Deposit Address\n3..Withdraw\n4..My All Transactions\n5..Log Out\n\n";
     int x;
     cin >> x;
     if (x == 1)
@@ -389,7 +596,8 @@ void log_in_load(Account *temp)
     }
     else if (x == 2)
     {
-        cout << "Deposit Address : " << temp->deposit_address << endl;
+        clear();
+        cout << "Deposit Address : " << temp->deposit_address << "\n\n";
         log_in_load(temp);
     }
     else if (x == 3)
@@ -402,6 +610,7 @@ void log_in_load(Account *temp)
     }
     else if (x == 5)
     {
+        clear();
         load();
     }
 }
